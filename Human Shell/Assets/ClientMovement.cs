0943d538286dbafb08f69386105b93884845df7b
Edouard Brooks <slights@weedle.cs.uchicago.edu>
2016-01-29 13:36:08 -0600

    ClientModel started.

---
+++ Human Shell/Assets/ClientMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClientMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
