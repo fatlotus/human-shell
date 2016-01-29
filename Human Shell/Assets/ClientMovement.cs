2c1a9d6e978d27efdf8766cdb58cb71b6f5405c4
Edouard Brooks <slights@weedle.cs.uchicago.edu>
2016-01-29 13:59:23 -0600

    Multiplayer added

--- Human Shell/Assets/ClientMovement.cs
+++ Human Shell/Assets/ClientMovement.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class ClientMovement : MonoBehaviour {
+public class ClientMovement : NetworkBehaviour {
+
+	private GameObject camera;
+	private float moveScale = 0.1f;
 
 	// Use this for initialization
 	void Start () {
-	
+		camera = GameObject.Find ("Scene_Cardboard");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		var x = Input.GetAxis ("Horizontal") * moveScale;
+		var z = Input.GetAxis ("Vertical") * moveScale;
+
+		transform.Translate (x, 0, z);
+		camera.transform.Translate (x, 0, z);
 	}
 }
