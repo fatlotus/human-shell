b53fb6d1ce3ea266e51eb663a05ecd2ccc8fd70d
Edouard Brooks <slights@weedle.cs.uchicago.edu>
2016-01-29 20:59:30 -0600

    Trying to network kinect to animator

---
+++ Human Shell/Assets/Scripts/ClientAnimation.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System.IO;
+using System.Net.Sockets;
+using System;
+
+public class ClientAnimation : NetworkBehaviour {
+
+	TcpClient 		mySocket;
+	NetworkStream 	theStream;
+	StreamWriter 	theWriter;
+	StreamReader 	theReader;
+	private Animator clientAnim;
+
+	// Use this for initialization
+	void Start () {
+		mySocket 	= new TcpClient ("linux3.cs.uchicago.edu", 9000);
+		theStream 	= mySocket.GetStream();
+		theWriter 	= new StreamWriter(theStream);
+		theReader 	= new StreamReader(theStream);
+
+		clientAnim = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		while (theStream.DataAvailable) {
+			string reading = theReader.ReadLine ();
+			print (reading);
+			string[] parts = reading.Split (new Char[] { ' ' });
+			Vector3 pos = new Vector3(float.Parse(parts [2]), float.Parse(parts [3]), float.Parse(parts [4]));
+
+			switch (parts [0]) {
+				case "head":
+					break;
+				case "lefthand":
+				clientAnim.SetIKPositionWeight (AvatarIKGoal.LeftHand, 1);
+					clientAnim.SetIKPosition(AvatarIKGoal.LeftHand, pos);
+					break;
+				case "righthand":
+				clientAnim.SetIKPositionWeight (AvatarIKGoal.RightHand, 1);
+					clientAnim.SetIKPosition(AvatarIKGoal.RightHand, pos);
+					break;
+				case "leftshoulder":
+					break;
+				case "rightshoulder":
+					break;
+			}
+		}
+
+		if (!isLocalPlayer)
+			return;
+	}
+}
