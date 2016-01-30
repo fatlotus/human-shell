9ca7c9496731f4f4f4bf7f5d3529206981a54991
Edouard Brooks <slights@weedle.cs.uchicago.edu>
2016-01-29 21:29:16 -0600

    Closer? Moving animator component

--- Human Shell/Assets/Scripts/ClientAnimation.cs
+++ Human Shell/Assets/Scripts/ClientAnimation.cs
@@ -15,6 +15,7 @@ public class ClientAnimation : NetworkBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		print ("Attached script.");
 		mySocket 	= new TcpClient ("linux3.cs.uchicago.edu", 9000);
 		theStream 	= mySocket.GetStream();
 		theWriter 	= new StreamWriter(theStream);
@@ -24,22 +25,23 @@ public class ClientAnimation : NetworkBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update() {
+	void OnAnimatorIK() {
 		while (theStream.DataAvailable) {
 			string reading = theReader.ReadLine ();
 			print (reading);
 			string[] parts = reading.Split (new Char[] { ' ' });
-			Vector3 pos = new Vector3(float.Parse(parts [2]), float.Parse(parts [3]), float.Parse(parts [4]));
+			Vector3 pos = new Vector3(float.Parse(parts [2]), float.Parse(parts [3]), float.Parse(parts [4]))/1000.0f;
+
 
 			switch (parts [0]) {
 				case "head":
 					break;
 				case "lefthand":
-				clientAnim.SetIKPositionWeight (AvatarIKGoal.LeftHand, 1);
+					clientAnim.SetIKPositionWeight (AvatarIKGoal.LeftHand, 1);
 					clientAnim.SetIKPosition(AvatarIKGoal.LeftHand, pos);
 					break;
 				case "righthand":
-				clientAnim.SetIKPositionWeight (AvatarIKGoal.RightHand, 1);
+					clientAnim.SetIKPositionWeight (AvatarIKGoal.RightHand, 1);
 					clientAnim.SetIKPosition(AvatarIKGoal.RightHand, pos);
 					break;
 				case "leftshoulder":
